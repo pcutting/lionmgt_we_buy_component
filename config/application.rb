92872a153a9dadb78b55d342e55d71e2f73ac6d3
Phil <phil@nevie.com>
2012-06-10 23:26:48 +0300

    working over video

--- config/application.rb
+++ config/application.rb
@@ -2,6 +2,9 @@ require File.expand_path('../boot', __FILE__)
 
 require 'rails/all'
 
+
+
+
 if defined?(Bundler)
   # If you precompile assets before deploying to production, use this line
   Bundler.require(*Rails.groups(:assets => %w(development test)))
@@ -11,6 +14,9 @@ end
 
 module Realestate
   class Application < Rails::Application
+  
+    
+    
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
